0ed520641c8ad2cd41ae215e46c97e2227eb63c3
Leyneryiret <4704@holbertonschool.com>
2022-02-15 11:45:15 -0800

    functions nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,8 @@
 /**
  * print_sign - prints the sign of a number
  *
+ *@n: la entrada es un numero entero.
+ *
  *Return: Returns 1 and prints + if n is greater than zero
  *Returns 0 and prints 0 if n is zero
  *Returns -1 and prints - if n is less than zero
