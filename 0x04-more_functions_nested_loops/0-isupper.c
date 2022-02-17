0531949037df928083ce7cc8d67cf955461fdd57
Leyneryiret <4704@holbertonschool.com>
2022-02-17 00:26:13 -0500

    more functions nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,19 @@
 /**
  * _isupper - checks for uppercase character
  *
- * @c: The character in char
+ * @c: variable type int
  *
  * Return: 1 for lowercase character. 0 for the rest.
  */
 
 int _isupper(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-      return (0);
-    }
-  else
-    {
-      return (1);
-    }
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+else
+{
+return (1);
+}
 }
