c74839f244edd9e7674c6acc0211842111638c55
Leyneryiret <4704@holbertonschool.com>
2022-02-17 16:36:20 -0500

    more function nested

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+/** 
+ *main - prints the numbers from 1 to 100,
+ *in  multiples of three print Fizz
+ *in multiples of five print Buzz
+ *in multiples of both three and five print FizzBuzz.
+ */
+int main(void)
+{
+  int n;
+
+for (n = 1; n < 101; n++)
+  {
+    if (n % 15 == 0)
+      {
+      printf("FizzBuzz ");
+      }
+      else if (n % 3 == 0)
+	{
+	  printf("Fizz ");
+	}
+      else if (n % 5 == 0)
+      printf("Buzz ");
+else
+    printf("%d ", n);
+  }
+ printf("\n");
+ return (0);
+}
