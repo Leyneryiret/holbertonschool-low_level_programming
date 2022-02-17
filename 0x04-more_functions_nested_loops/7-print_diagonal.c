24046ff5e689667f65e4608296218c074fbce33b
Leyneryiret <4704@holbertonschool.com>
2022-02-17 15:00:32 -0500

    more function

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,14 @@ void print_diagonal(int n)
 int e;
 char c;
 
-if (n <=0)
+if (n <= 0)
 _putchar('\n');
 
 for (c = 0; c < n; c++)
 {
 for (e = 0; e <= c; e++)
 {
-if(e == c)
+if (e == c)
 {
 _putchar(92);
 _putchar('\n');
