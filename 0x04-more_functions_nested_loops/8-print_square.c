0fa800cbcdefc1eeb2153758934958af1617ac42
Leyneryiret <4704@holbertonschool.com>
2022-02-17 15:29:44 -0500

    more functions nested

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,19 +6,19 @@
 
 void print_square(int size)
 {
-  int alto;
+int alto;
 int largo;
-  
-  if (size <= 0)
-    _putchar('\n');
+
+if (size <= 0)
+_putchar('\n');
 for (alto = 0; alto < size; alto++)
-  {
-    for (largo = 0; largo <= size; largo++)
-      {
-	if (largo == size)
-	  _putchar('\n');
-	else
-	  _putchar(35);
-      }
-  }
+{
+for (largo = 0; largo <= size; largo++)
+{
+if (largo == size)
+_putchar('\n');
+else
+_putchar(35);
+}
+}
 }
