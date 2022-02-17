eb797e3aec809542ae35fcb4859b27f98c0d66ba
Leyneryiret <4704@holbertonschool.com>
2022-02-17 00:14:53 -0500

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Write a function that draws a straight line in the terminal.
+ * @n: Integer amount of _
+ */
+
+void print_diagonal(int n)
+{
+char c;
+
+for (c = 0; c < n; c++)
+  if (n != 0)
+    _putchar(92);
+_putchar(92);
+_putchar('\n');
+}
