f0350c327628f7f73359c5eae5ae27b4b931540f
Leyneryiret <4704@holbertonschool.com>
2022-02-21 13:56:57 -0500

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - toma un puntero a un parámetro y actualiza su valor.
+ * @n: entero de entrada.
+ * Devolución: sin devolución.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
