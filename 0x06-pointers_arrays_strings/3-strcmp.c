66b60f3f75550adeb4cf1cfd3dcd8a3b9632ce85
Leyneryiret <4704@holbertonschool.com>
2022-02-24 14:26:08 -0500

    MORE ARRAYS

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int r = 0;
+int i = 0, r = 0;
 
-r = *s1 - *s2;
+while (r == 0)
+{
+if ((s1[i] == '\0') && (s2[i] == '\0'))
+break;
+r = s1[i] - s2[i];
+i++;
+}
 return (r);
 }
