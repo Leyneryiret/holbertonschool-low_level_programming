144f97fcc9a08ecd0f2673977e3409dc2242fab3
Leyneryiret <4704@holbertonschool.com>
2022-02-24 14:06:05 -0500

    MORE ARRAYS

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strcmp - function that compares two strings.
+ *@s1: is a tipe array.
+ *@s2: is a tipe array.
+ *Return: @r un tioe integer
  */
+
 int _strcmp(char *s1, char *s2)
 {
 int r = 0;
+
 r = *s1 - *s2;
 return (r);
 }
