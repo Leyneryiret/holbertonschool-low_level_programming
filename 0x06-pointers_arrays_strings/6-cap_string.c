dbc088921214c3f793e40b47c435c1efb610ade8
Leyneryiret <4704@holbertonschool.com>
2022-02-24 18:40:24 -0500

    MORE ARRAYS

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @a: is a array tipe char.
+ * Return: the poin to a
+ */
+
+char *cap_string(char *a)
+{
+int s[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+int i = 0, j = 0;
+
+while (a[i] != '\0')
+{
+for (j = 0; j <= 12; j++)
+{
+if (a[i] == s[j])
+{
+if (a[i + 1] >= 97 && a[i + 1] <= 122)
+{
+a[i + 1] = a[i + 1] - 32;
+break;
+}
+}
+}
+i++;
+}
+return (a);
+}
