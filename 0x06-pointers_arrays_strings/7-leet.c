d4d2e6e5454c398e5d3ca13a034f0cd30a8acf51
Leyneryiret <4704@holbertonschool.com>
2022-02-24 22:14:50 -0500

    MORE ARRAYS

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,22 @@
 
 char *leet(char *s)
 {
-int i = 0;
+  int lm[] = {97, 101, 111, 116, 108};
+  int l[] = {65, 69, 79, 84, 76};
+  int rem[] = {52, 51, 48, 55, 49};
+  int i = 0, j;
 
-while (s[i] != '\0')
-{
-if (s[i] == 65 || s[i] == 97)
-s[i] = 52;
-else if (s[i] == 69 || s[i] == 101)
-s[i] = 51;
-else if (s[i] == 79 || s[i] == 111)
-s[i] = 48;
-else if (s[i] == 84 || s[i] == 116)
-s[i] = 55;
-else if (s[i] == 76 || s[i] == 108)
-s[i] = 49;
-else
-s[i] = s[i];
-i++;
-}
-return (s);
+  while (s[i] != '\0')
+    {
+      for (j = 0; j < 5; j++)
+	{
+	  if (s[i] == lm[j] || s[i] == l[j])
+	    {
+	      s[i] = rem[j];
+	      break;
+	    }
+	}
+      i++;
+    }
+  return (s);
 }
