6f8c26fe1ca05cc9b44f2f22247b81c335ef1f99
Leyneryiret <4704@holbertonschool.com>
2022-03-04 16:16:00 -0500

    argc-argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,11 @@ int main(int argc, char *argv[])
 {
 int a, b, resultado;
 
-if (argc == 3)
-{
+if (argc <= 2)
+printf("Error\n");
+return (1);
 a = atoi(argv[1]), b = atoi(argv[2]);
 resultado = a *b;
 printf("%d\n", resultado);
 }
-else
-printf("Error\n");
-return (1);
-}
+
