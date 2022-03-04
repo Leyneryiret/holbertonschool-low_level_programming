61fc0aadd12bff709b7e6e0dcbc2dc2f339d0028
Leyneryiret <4704@holbertonschool.com>
2022-03-04 16:17:59 -0500

    argc-argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
 * main -  writes its name
 * @argc: arguement count
@@ -16,5 +17,6 @@ return (1);
 a = atoi(argv[1]), b = atoi(argv[2]);
 resultado = a *b;
 printf("%d\n", resultado);
+return (0);
 }
 
