fc3e769a38e147cf2042a78e46baa8af9569df2a
Leyneryiret <4704@holbertonschool.com>
2022-03-08 21:37:38 -0500

    argc argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints the minimum number of coins to make change for an amount.
  * of money.
@@ -8,42 +7,32 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
-int cents, sum;
+int cents, ncoins = 0;
 
-if (argc != 2)
+if (argc == 1 || argc > 2)
 {
 printf("Error\n");
 return (1);
 }
-sum = 0;
+
 cents = atoi(argv[1]);
-if (cents > 25)
+
+while (cents > 0)
 {
-while (cents >= 25)
-cents -= 25, sum++;
+if (cents >= 25)
+cents -= 25;
+else if (cents >= 10)
+cents -= 10;
+else if (cents >= 5)
+cents -= 5;
+else if (cents >= 2)
+cents -= 2;
+else if (cents >= 1)
+cents -= 1;
+ncoins += 1;
 }
-if (cents > 10 && cents < 25)
-{
-while (cents >= 10)
-cents -= 10, sum++;
-}
-if (cents > 5 && cents < 10)
-{
-while (cents >= 5)
-cents -= 5, sum++;
-}
-if (cents > 2 && cents < 5)
-{
-while (cents >= 2)
-cents -= 2, sum++;
-}
-if (cents == 1 || cents == 2 || cents == 5 || cents == 10 || cents == 25)
-{
-sum++;
-}
-printf("%d\n", sum);
-return (sum);
+printf("%d\n", ncoins);
+return (0);
 }
