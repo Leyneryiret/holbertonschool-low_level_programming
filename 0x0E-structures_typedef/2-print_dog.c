6e9067daf1aaa8f8213e8815e8a4d5052b7ca18b
Leyneryiret <4704@holbertonschool.com>
2022-03-15 17:07:07 -0500

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,17 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-if (((*d).name) == NULL)
+if (!(*d).name)
+{
 printf("Name: (nil)\n");
 printf("Name: %s\n", (*d).name);
-
+}
 printf("Age: %f\n", (*d).age);
 
-if (((*d).owner) == NULL)
+if (!(*d).owner)
+{
 printf("Owner: (nil)\n");
 printf("Owner: %s\n", (*d).owner);
+}
 }
 }
