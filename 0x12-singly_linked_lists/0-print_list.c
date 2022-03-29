31d844a7ff4e6f40f695820f2eb8c487ce71040d
Leyneryiret <4704@holbertonschool.com>
2022-03-29 12:31:22 -0500

    listas enlazadas

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-  size_t i = 0;
+size_t i = 0;
 
-  if (h->str == NULL)
-    {
-      printf ("[0] (nil)\n");
-      h = h->next;
-      i++;
-    }
-  for (; h; i++)
-    {
-      printf("[%d] %s\n", h->len, h->str);
-      h = h->next;
-    }
-  return (i);
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+h = h->next;
+i++;
+}
+for (; h; i++)
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+}
+return (i);
 }
