9d3b6dbff7b81ffe27c4b495c91d94ff864d0445
Leyneryiret <4704@holbertonschool.com>
2022-03-31 13:56:43 -0500

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints all elements of a list_t list
+ * @h: singly linked list to print
+ * Return: number of nodes in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t i = 0;
+
+if (!h)
+return (0);
+
+for (; h; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
+}
