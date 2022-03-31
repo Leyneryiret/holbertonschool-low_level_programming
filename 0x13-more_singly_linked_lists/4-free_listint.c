ec6e9e73b57ef48f213d7f8fe5a62d826bc11e4e
Leyneryiret <4704@holbertonschool.com>
2022-03-31 18:45:14 -0500

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: the beginning of the list
+ */
+
+void free_listint(listint_t *head)
+{
+if (head == NULL)
+return;
+free_listint(head->next);
+free(head);
+}
