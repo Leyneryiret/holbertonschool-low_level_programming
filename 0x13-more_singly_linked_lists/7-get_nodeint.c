bedf25ec34102107c36d4081e2acb2347019b416
Leyneryiret <4704@holbertonschool.com>
2022-04-01 15:37:34 -0500

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i = 0;
 
-if (head == NULL)
+for (i = 0; i < index; i++)
+{
+if (head->next == NULL)
 return (NULL);
 else
-{
-for (i = 0; i < index; i++)
 head = head->next;
 }
 return (head);
