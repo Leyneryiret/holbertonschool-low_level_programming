4beb61ae1b1898fa4ccd5c0fe4721ea0c3a89c80
Leyneryiret <4704@holbertonschool.com>
2022-05-12 11:51:18 -0500

    agregar nodos al principio

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,21 +7,21 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-  dlistint_t *new = NULL;
+	dlistint_t *new = NULL;
 
-  new = malloc(sizeof(dlistint_t));
-  if (new == NULL)
-    return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-  new->n = n;
-  new->prev = NULL;
-  new->next = NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
 
-  if (*head != NULL)
-    {
-      new->next = *head;
-      (*head)->prev = new;
-    }
-  *head = new;
-  return (new);  
+	if (*head != NULL)
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
+	*head = new;
+	return (new);
 }
