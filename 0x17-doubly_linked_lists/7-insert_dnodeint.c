2b89be3a60273a8d5c376e215faeafcf8717ad7e
Leyneryiret <4704@holbertonschool.com>
2022-05-12 17:38:56 -0500

    insertar

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = NULL, *aux = *h;
 	unsigned int copy = idx, cont = 0;
 
-	if (*h == NULL)
+	if (*h == NULL && idx > 0)
 		return (NULL);
 	if (idx == 0)
 	{
