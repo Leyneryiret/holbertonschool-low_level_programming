3c2ddd4543ec1a7d22991576988e8dd2d58c70a3
leyneryiret <4704@holbertonschool.com>
2022-06-06 21:17:12 -0500

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,20 +12,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *actual_node;
 
-	if (!ht && !key)
-		return (NULL);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	actual_node = ht->array[index];
-
-	while (actual_node)
+	if (ht && key)
 	{
-	  if (strcmp(actual_node->key, key) == 0)
-			return (actual_node->value);
+		index = key_index((const unsigned char *)key, ht->size);
 
-		actual_node = actual_node->next;
+		actual_node = ht->array[index];
+
+		while (actual_node)
+		{
+			if (strcmp(actual_node->key, key) == 0)
+				return (actual_node->value);
+
+			actual_node = actual_node->next;
+		}
 	}
-
 	return (NULL);
 }
